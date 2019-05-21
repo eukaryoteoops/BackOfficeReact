c57be8db18316eb1c1d5b81571996550549985e5
TonyChang <tony.chang@phoenixnet.com.tw>
2019-05-21 18:57:23 +0800

    Init

---
+++ src/views/Order/Order.js
@@ -0,0 +1,215 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardHeader, Col, Row, Table, FormGroup, Label, Input, Button } from 'reactstrap';
+import axios from 'axios';
+import RCPagination from "react-js-pagination";
+import { DateRangePicker } from 'react-date-range';
+
+class Order extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+            total: 0,
+            activePage: 1,
+            formData: {
+                nickName: '',
+                code: '',
+                method: '',
+                item: '',
+                startDate: new Date(),
+                endDate: new Date(),
+                status: 4
+            }
+        };
+        this.getOrderList = this.getOrderList.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleDatePick = this.handleDatePick.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    getOrderList() {
+        let self = this;
+        axios.get('http://uat.bo.snf.today/BrandPlatform/Order',
+            {
+                headers: { "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIzIiwianRpIjoiZTlhMjRhNjgtMjc1NC00MTY3LWE3ZjgtN2JmMjEzMDVlNTRhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkZha2UgQWRtaW4gVXNlciIsImxhbmciOiJ6aC1UVyIsImJyYW5kSWQiOiJTTkYiLCJWaXJ0dWFsVXNlciI6IkZhbHNlIiwiVklQVXNlciI6IkZhbHNlIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE1NTg0NDgyMjMsImlzcyI6IjQxNGUxOTI3YTM4ODRmNjhhYmM3OWY3MjgzODM3ZmQxIiwiYXVkIjoicGhvZW5peGludGVyYWN0aXZlIn0.-htufPWSBQ4ODoAFTor771zpgMgGEODT3qT_ToOqiEI` },
+                params: {
+                    nickName: this.state.formData.nickName,
+                    channelName: this.state.formData.code,
+                    paymethodId: this.state.formData.method,
+                    productId: this.state.formData.item,
+                    beginDate: this.state.formData.startDate,
+                    endDate: this.state.formData.endDate,
+                    pageNo: this.state.activePage,
+                    pageSize: 20,
+                    status: this.state.formData.status
+                }
+            })
+            .then(function (response) {
+                self.setState({
+                    list: response.data.dataList,
+                    total: response.data.amount
+                });
+                window.scrollTo(0, 480);
+            });
+    }
+
+
+    handlePageChange(e) {
+        this.setState({
+            activePage: e
+        }, () => { this.getOrderList() })
+
+    }
+
+    handleDatePick(ranges) {
+        this.setState({
+            formData: { ...this.state.formData, startDate: ranges.selection.startDate, endDate: ranges.selection.endDate }
+        })
+    }
+
+    handleInputChange(event) {
+        event.persist();
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            formData: { ...this.state.formData, [name]: value }
+        });
+    }
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col md="6">
+                        <FormGroup className="pr-1" row>
+                            <Col md="2"><Label htmlFor="nickName" className="pr-1">會員暱稱</Label></Col>
+                            <Col md="10"><Input name="nickName" type="text" placeholder="王小明..." onChange={this.handleInputChange} /></Col>
+                        </FormGroup>
+                        <FormGroup className="pr-1" row>
+                            <Col md="2"><Label htmlFor="code" className="pr-1">金流代號</Label></Col>
+                            <Col md="10"><Input name="code" type="text" placeholder="BeeBuy..." onChange={this.handleInputChange} /></Col>
+                        </FormGroup>
+                        <FormGroup className="pr-1" row>
+                            <Col md="2"><Label htmlFor="method" className="pr-1">購買方式</Label></Col>
+                            <Col md="10"><Input name="method" type="text" placeholder="微信..." onChange={this.handleInputChange} /></Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col md="2">
+                                <Label htmlFor="item">購買項目</Label>
+                            </Col>
+                            <Col md="10">
+                                <Input type="select" name="item" onChange={this.handleInputChange}>
+                                    <option value="0">請選擇</option>
+                                    <option value="1">One Time</option>
+                                    <option value="2">Discount</option>
+                                    <option value="3">Download</option>
+                                    <option value="4">PayBack</option>
+                                    <option value="5">VIP</option>
+                                </Input>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col md="2">
+                                <Label htmlFor="status">購買狀態</Label>
+                            </Col>
+                            <Col md="10">
+                                <Input type="select" name="status" onChange={this.handleInputChange} defaultValue="4">
+                                    <option value="0">初始中</option>
+                                    <option value="1">處理中</option>
+                                    <option value="2">已支付</option>
+                                    <option value="3">異常</option>
+                                    <option value="4">已完成</option>
+                                </Input>
+                            </Col>
+                        </FormGroup>
+                    </Col>
+                    <Col md="6">
+                        <FormGroup row>
+                            <Col md="2">
+                                <Label htmlFor="range">購買區間</Label>
+                            </Col>
+                            <Col md="10">
+                                <DateRangePicker
+                                    ranges={[{
+                                        startDate: this.state.formData.startDate,
+                                        endDate: this.state.formData.endDate,
+                                        key: 'selection',
+                                    }]}
+                                    onChange={this.handleDatePick}
+                                />
+                            </Col>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row className="align-items-center">
+                    <Col className="text-center">
+                        <FormGroup>
+                            <Button className="align-items-center" size="lg" color="primary" onClick={this.getOrderList}><i className="fa fa-dot-circle-o"></i> Submit</Button>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="12" lg="12">
+                        <Card>
+                            <CardHeader>
+                                <i className="fa fa-align-justify"></i> 訂單查詢
+                            </CardHeader>
+                            <CardBody>
+                                <Table responsive striped>
+                                    <thead>
+                                        <tr>
+                                            <th>項次</th>
+                                            <th>會員暱稱</th>
+                                            <th>金流代號</th>
+                                            <th>購買方式</th>
+                                            <th>購買金額</th>
+                                            <th>購買項目</th>
+                                            <th>交易編號</th>
+                                            <th>金流編號</th>
+                                            <th>購買時間</th>
+                                            <th>狀態</th>
+                                            <th>購買域名</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.list.map((data, i) => {
+                                            return (
+                                                <tr key={i}>
+                                                    <td>{i + 1}</td>
+                                                    <td>{data.nickName}</td>
+                                                    <td>{data.paymethodName}</td>
+                                                    <td>{data.channelName}</td>
+                                                    <td>{data.amount}</td>
+                                                    <td>{data.productName}</td>
+                                                    <td>{data.orderId}</td>
+                                                    <td>{data.outOrderId}</td>
+                                                    <td>{new Date(data.purchasedDate * 1000).toLocaleString()}</td>
+                                                    <td>{data.status}</td>
+                                                    <td>{data.purchasedDomain}</td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </Table>
+                                <RCPagination
+                                    hideDisabled
+                                    pageRangeDisplayed={10}
+                                    activePage={this.state.activePage}
+                                    itemsCountPerPage={20}
+                                    totalItemsCount={this.state.total}
+                                    onChange={this.handlePageChange}
+                                    linkClass={'page-link'}
+                                    itemClass={'page-item'}
+                                    innerClass={'pagination justify-content-center'}
+                                />
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div >
+        );
+    }
+}
+
+export default Order;
---
+++ src/views/Platform/Platform.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Platform extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                Platform
+            </div>
+        );
+    }
+}
+
+export default Platform;
